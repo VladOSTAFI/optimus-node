d7af96f3315f5ba3ecfc19e0e9111be45547e642
vostafi-impressit <Vlad.Ostafi@g.network>
2023-08-08 19:35:37 +0300

    added theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@mui/material';
+import { theme } from '@/styles/theme';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +14,9 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeProvider theme={theme}>
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
     </html>
   );
 };
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#232323',
+    },
+  },
+});
